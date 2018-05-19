4e9907ce416c83ad7402c8c37bdebd4f2a44e6c0
sezingumus <sezingumus@gmail.com>
2018-05-20 00:09:17 +0300

    fixed min()

--- TSP.py
+++ TSP.py
@@ -10,15 +10,15 @@ class City(object):
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
         self.visited = False
-        self.neighbor = []
+        self.neighbors = []
  
     def setIsVisit(self,visited):
         self.visited = visited         
     
     def addNeighbors(self,ID,distance):
-        self.neighbor.append([ID,distance])
+        self.neighbors.append([ID,distance])
     def getNeighbors(self):
-        return self.neighbor
+        return self.neighbors
     
 def fileToList(input_file):
     file = open(input_file,"r")
@@ -29,7 +29,8 @@ def fileToList(input_file):
         list_of_cities.append(City(_id,int(x),int(y)))
         line = file.readline()
     file.close()
-    return list_of_cities
+    boundNeighborsToACity(list_of_cities)
+    #return list_of_cities
     
 def calculateDistance(x1,x2,y1,y2):
         x = (x1-x2) * (x1-x2)
@@ -38,27 +39,20 @@ def calculateDistance(x1,x2,y1,y2):
         d = round(d)
         return d
 
-
-def listToAdjacencyMatrix(city_list):
- length = len(city_list)    
- adjmatrix = [[0]*length for i in range(length)] 
- neighbor = []
+def boundNeighborsToACity(list_of_cities):
+ length = len(list_of_cities)     
  for i in range(0,length):
      for j in range(0,length):
-        distance = calculateDistance(city_list[j].x_coordinate, city_list[i].x_coordinate , city_list[j].y_coordinate, city_list[i].y_coordinate)
-        adjmatrix[i][j] = distance
+        distance = calculateDistance(list_of_cities[j].x_coordinate, list_of_cities[i].x_coordinate ,list_of_cities[j].y_coordinate, list_of_cities[i].y_coordinate)
         if distance != 0:
-            city_list[i].addNeighbors(city_list[j].ID,distance)
+            list_of_cities[i].addNeighbors(list_of_cities[j].ID,distance)
 
- for i in range(len(city_list)):
+
+def nearestNeighbor (list_of_cities):
+   for i in range(len(list_of_cities)):
      if city_list[i].ID == 'a':
-         #print(city_list[i].x_coordinate,city_list[i].y_coordinate,city_list[i].visited)
-         print(min(city_list[i].getNeighbors()))
-         
- return adjmatrix
+         print(min(list_of_cities[i].getNeighbors(), key=lambda x: x[1]))         
 
-def findNearestNeighbor (city):
-  pass
     
 
 def listtoFile(output_file):
@@ -67,11 +61,13 @@ def listtoFile(output_file):
         file.write("exp \n")
  file.close()
     
-cities = fileToList("test1.txt")
-adj_matrix = listToAdjacencyMatrix(cities)
+
+fileToList("test1.txt")
+#boundNeighborsToACity(cities)
 
 
-#adj = listtoAdjacencyMatrix("test1.txt")#listtoAdjacencyMatrix(sys.argv[1]) 
+
+#fileToList(sys.argv[1]) 
 
     
    
